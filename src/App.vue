08ddc5d549d53948710d772adc39e2a169b77d1c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 15:50:26 -0400

    Renderizado condicional, permite mostrar o no un contenido dependiendo de una condición, se usa v-if como atributo dentro de las etiquetas html

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-    // Para trabajar con propiedades computadas es necesario importar la definición computed
-    import { computed } from 'vue';
-
-    const ok = true;
-
-    // Las propiedades computadas siempre deben retornar un valor y computed recibe un callback
-    const isOk = computed(() => ok ? 'Si' : 'No');
-
-    /* Las funciones tradicionales pueden conseguir el mismo efecto que las propiedades computadas, pero la diferencia fundamental es que las propiedades computadas almacenan en caché los resultados, es decir, los cálculos solo se hacen la primera vez que se llama, las funciones calculan en cada llamada */
-    // const isOk = () => ok ? 'Si' : 'No';
+    const awesome = true;
 </script>
 
 <template>
-    <!-- Las propiedades computadas funcionan muy similar a un método, pero no se llama con paréntesis -->
-    {{ isOk }}
+    <!-- Muestra el contenido solo si la condición es verdadera -->
+    <p v-if="awesome">
+        MHenriquez es asombroso
+    </p>
 </template>
 
 <style scoped>
