530a94dc873e69f657b7290cc1b44de65cbd295c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 12:50:31 -0400

    Se llevó a funciones el incremento y decremento del contador y se vió el uso de la función ref que es convertir en un objeto la variable a la que hace referencia, por eso se debe utilizar la propiedad value para manipularla en esos casos

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,23 @@
 
     // Esta función envuelve un objeto o variable dentro de una referencia con propiedad .value
     let cont = ref(0);
+
+    const decrement = () => {
+        cont.value--;
+        console.log(cont.value);
+    }
+
+    const increment = () => {
+        cont.value++;
+        console.log(cont.value);
+    }
 </script>
 
 <template>
     <!-- Una vez definida la referencia, se puede mutar su valor facilmente -->
-    <button type="button" @click="cont--">-</button>
+    <button type="button" @click="decrement">-</button>
     {{ cont }}
-    <button type="button" @click="cont++">+</button>
+    <button type="button" @click="increment">+</button>
 </template>
 
 <style scoped>
