d0db7bf69b3bd3b00f80643418795152e838d2a3
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 15:39:33 -0400

    Se replicó la propiedad computada con una función tradicional, principalmente la diferencia es que no se llama con () y que se debe importar el objeto computed, pero además guarda los resultados dentro de la caché, lo que optimiza el rendimiento al calcular solo la primera vez si se llama varias veces, las siguientes consultarán la caché

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
     // Para trabajar con propiedades computadas es necesario importar la definición computed
     import { computed } from 'vue';
 
-    const ok = false;
+    const ok = true;
 
     // Las propiedades computadas siempre deben retornar un valor y computed recibe un callback
     const isOk = computed(() => ok ? 'Si' : 'No');
+
+    /* Las funciones tradicionales pueden conseguir el mismo efecto que las propiedades computadas, pero la diferencia fundamental es que las propiedades computadas almacenan en caché los resultados, es decir, los cálculos solo se hacen la primera vez que se llama, las funciones calculan en cada llamada */
+    // const isOk = () => ok ? 'Si' : 'No';
 </script>
 
 <template>
-    <!-- Las propiedades computadas funcionan muy similar a un método -->
+    <!-- Las propiedades computadas funcionan muy similar a un método, pero no se llama con paréntesis -->
     {{ isOk }}
 </template>
 
