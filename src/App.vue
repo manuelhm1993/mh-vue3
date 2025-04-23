0d5393787324db122240e8e6564b81ee5a355e56
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 18:44:31 -0400

    Se utilizó un bucle anidado para mostrar alumnos y sus cursos, para llevar un orden se usaron los index de cada item, además de los template strings dentro de la interpolación

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,109 @@
 <script setup>
     const alumnos = [
-        {nombre: 'Manuel', edad: 31},
-        {nombre: 'Luna', edad: 27},
-        {nombre: 'Mariandry', edad: 24},
-        {nombre: 'Santiago', edad: 24},
-        {nombre: 'Paola', edad: 30}
+        {
+            nombre: 'Manuel', 
+            edad: 31,
+            cursos: [
+                {
+                    nombre: 'Matemáticas',
+                    nota: 20,
+                },
+                {
+                    nombre: 'Física',
+                    nota: 15,
+                },
+                {
+                    nombre: 'Química',
+                    nota: 18,
+                },
+            ],
+        },
+        {
+            nombre: 'Luna', 
+            edad: 27,
+            cursos: [
+                {
+                    nombre: 'Matemáticas',
+                    nota: 20,
+                },
+                {
+                    nombre: 'Física',
+                    nota: 15,
+                },
+                {
+                    nombre: 'Química',
+                    nota: 18,
+                },
+            ],
+        },
+        {
+            nombre: 'Mariandry', 
+            edad: 24,
+            cursos: [
+                {
+                    nombre: 'Matemáticas',
+                    nota: 20,
+                },
+                {
+                    nombre: 'Física',
+                    nota: 15,
+                },
+                {
+                    nombre: 'Química',
+                    nota: 18,
+                },
+            ],
+        },
+        {
+            nombre: 'Santiago', 
+            edad: 24,
+            cursos: [
+                {
+                    nombre: 'Matemáticas',
+                    nota: 20,
+                },
+                {
+                    nombre: 'Física',
+                    nota: 15,
+                },
+                {
+                    nombre: 'Química',
+                    nota: 18,
+                },
+            ],
+        },
+        {
+            nombre: 'Paola', 
+            edad: 30,
+            cursos: [
+                {
+                    nombre: 'Matemáticas',
+                    nota: 20,
+                },
+                {
+                    nombre: 'Física',
+                    nota: 15,
+                },
+                {
+                    nombre: 'Química',
+                    nota: 18,
+                },
+            ],
+        },
     ];
 </script>
 
 <template>
     <ul>
         <!-- Desestructurar el objeto -->
-        <li v-for="({ nombre, edad }, i) in alumnos">
-            {{ i + 1 }}. {{ nombre }} - {{ edad }}
+        <li v-for="({ nombre, edad, cursos }, i) in alumnos">
+            <p>{{ i + 1 }}. {{ nombre }} - {{ edad }}</p>
+            <ul>
+                <li v-for="(curso, j) in cursos">
+                    <!-- Uso de template strings dentro de la interpolación vue -->
+                    {{ `${i + 1}.${j + 1}.` }} {{ curso.nombre }} - {{ curso.nota }}
+                </li>
+            </ul>
         </li>
     </ul>
 </template>
