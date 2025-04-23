4f4e3820851c057c22b42564a5fb76e577afc28e
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 13:03:51 -0400

    Trabajando con objetos reactivos y ref

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,26 @@
     // Para declarar la reactividad dentro de composition, se usa la función ref
     import { ref } from 'vue';
 
-    // Esta función envuelve un objeto o variable dentro de una referencia con propiedad .value
-    let cont = ref(0);
+    // Al trabajar con objetos, ref no es la mejor opción
+    let state = ref({
+        cont: 0
+    });
 
     const decrement = () => {
-        cont.value--;
+        // Al trabajar con objetos, se debe acceder a las propiedades luego del value
+        state.value.cont--;
     }
 
     const increment = () => {
-        cont.value++;
+        state.value.cont++;
     }
 </script>
 
 <template>
     <!-- Una vez definida la referencia, se puede mutar su valor facilmente -->
-    <button type="button" @click="decrement" :disabled="cont === 0">-</button>
+    <button type="button" @click="decrement" :disabled="state.cont === 0">-</button>
     <!-- Aquí no se coloca el .value porque del lado de html vue lo hace por detrás -->
-    {{ cont }}
+    {{ state.cont }}
     <button type="button" @click="increment">+</button>
 </template>
 
