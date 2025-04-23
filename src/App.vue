50fde9baaca08091138e44fcdd94008daa178e2a
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 16:33:27 -0400

    Condición que afecta a varios elementos

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,13 @@
     <button type="button" @click="setType">B</button>
     <button type="button" @click="setType">C</button>
 
+    <!-- Si se desea mostrar varios elementos en función de una condición se puede agregar el if a todos -->
+    <p v-if="type === 'A'">Esta es</p>
+    <p v-if="type === 'A'">la etiqueta</p>
     <p v-if="type === 'A'">A</p>
+
     <p v-else-if="type === 'B'">B</p>
+
     <p v-else>C</p>
 </template>
 
