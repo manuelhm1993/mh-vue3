2b8cf7bada06218fd311c85fe3f3d5d692245185
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 11:11:22 -0400

    Quitar el atributo setup dentro de la etiqueta script nos obliga a no trabajar la sintaxis abreviada y exportar nuestras variables y funciones dentro de la función setup, se recomienda el método abreviado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <!-- Estructura de un componente Vue -->
 <!--  -->
 <!-- Script - Tiene la lógica js (opcional) -->
-<script setup>
+<!-- <script setup>
     // El atributo setup indica que se está trabajando con composition y sintaxis abreviada
     const msg = 'Hola mundo';
+</script> -->
+
+<!-- Si no se coloca el atrubuto setup se debe definir la lófica dentro de la función setup y exportar -->
+<script>
+    export default {
+        setup() {
+            const msg = 'Hola mundo';
+
+            const saludo = () => console.log('Hola desde vue3');
+
+            return {
+                msg, saludo
+            };
+        }
+    }
 </script>
 
 <!-- Template - Tiene el código html que será dinámico (obligatorio) -->
 <template>
     <!-- Los atributos o parámetros se muestran igual que en blade con la sintaxis {{  }} -->
     <h1>{{ msg }}</h1>
+    {{ saludo() }}
 </template>
 
 <!-- Style - Define los estilos css aplicados al template (opcional) -->
