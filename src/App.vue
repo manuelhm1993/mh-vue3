6e7cef706195496610563fdd32e0309c1041d959
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 16:03:49 -0400

    Se puede usar la combinación if-else en vue, esto añade mucha potencia al desarrollo front porque ahorra mucho tiempo y trabajo

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@
     <p v-if="awesome">
         MHenriquez es asombroso
     </p>
+
+    <!-- Renderizado en caso de que la condición sea falsa, el v-else debe ir inmediatamente a continuación de un v-if o v-else-if -->
+    <p v-else>
+        Haz click :D
+    </p>
 </template>
 
 <style scoped>
