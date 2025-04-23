3cb6d424440fb79a31b604885b80d8c11e4372f8
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-23 15:32:03 -0400

    Las propiedades computadas permiten escalar, modularizar y encapsular el código, es muy similar a una función, pero tiene bondades adicionales

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script setup>
-    // Para declarar la reactividad dentro de composition, se usa la función ref (primitivos)
-    // Para declarar la reactividad dentro de composition, se usa la función rective (objetos)
-    import { reactive } from 'vue';
+    // Para trabajar con propiedades computadas es necesario importar la definición computed
+    import { computed } from 'vue';
 
-    // Al trabajar con objetos se debe usar reactive simplemente convertir el objeto en reactivo y no envolver
-    let state = reactive({
-        cont: 0
-    });
+    const ok = false;
 
-    const decrement = () => {
-        // Al trabajar con objetos, se debe acceder a las propiedades luego del value
-        state.cont--;
-    }
-
-    const increment = () => {
-        state.cont++;
-    }
+    // Las propiedades computadas siempre deben retornar un valor y computed recibe un callback
+    const isOk = computed(() => ok ? 'Si' : 'No');
 </script>
 
 <template>
-    <!-- Una vez definida la referencia, se puede mutar su valor facilmente -->
-    <button type="button" @click="decrement" :disabled="state.cont === 0">-</button>
-    <!-- Aquí no se coloca el .value porque del lado de html vue lo hace por detrás -->
-    {{ state.cont }}
-    <button type="button" @click="increment">+</button>
+    <!-- Las propiedades computadas funcionan muy similar a un método -->
+    {{ isOk }}
 </template>
 
 <style scoped>
