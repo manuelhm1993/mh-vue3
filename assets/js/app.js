d582e61bbe46731d1e26ce18f0ac7fd5621b2fa0
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 16:15:25 -0400

    Uso de la palabra reservada let

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var nombre = 'Manuel'; // Antiguamente para definir variables se utilizaba la palabra reservada var
-var nombre = 'Luna'; // La palabra var permitia redefinir una variable y modificar su valor, también es de ámbito global
+let nombre = 'Manuel'; // Actualmente las variables se declaran con let y las constantes con const
+let nombre2 = 'Luna'; // La palabra let permite crear variables y modificar su valor, pero no redefinirlas, además son de ámbito local
 
 console.log(nombre);
