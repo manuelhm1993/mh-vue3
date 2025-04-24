3e3fc1d7675f95af6fd6d4c951e11fb3fde017af
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 10:45:53 -0400

    Forma básica de manejar eventos, usando la directiva v-on y el método abreviado @

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <script setup>
-    const alumnos = [
-        {
-            id: 1,
-            nombre: 'Manuel', 
-            edad: 31,
-        },
-        {
-            id: 2,
-            nombre: 'Luna', 
-            edad: 27,
-        },
-        {
-            id: 3,
-            nombre: 'Mariandry', 
-            edad: 24,
-        },
-        {
-            id: 4,
-            nombre: 'Santiago', 
-            edad: 24,
-        },
-        {
-            id: 5,
-            nombre: 'Paola', 
-            edad: 30,
-        },
-    ];
+    
 </script>
 
 <template>
-    <ul>
-        <!-- Cuando se usen CRUDs si no se identifican los items podrá fallar, en ese caso un id es necesario -->
-        <li v-for="alumno in alumnos" :key="alumno.id">
-            {{ alumno.nombre }} - {{ alumno.edad }}
-        </li>
-    </ul>
+    <!-- Forma básica de manejar eventos -->
+    <!-- <button type="button" v-on:click="console.log('Hola desde vue')"> -->
+    
+    <!-- Método abreviado -->
+    <button type="button" @click="console.log('Hola desde vue')">
+        Haz click
+    </button>
 </template>
 
 <style scoped>
