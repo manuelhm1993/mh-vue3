a8a445ec1b5b50a4ee2efcf37f2a2d5dc6bcbd08
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 12:20:59 -0400

    Uso de v-model para trabajar con checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,45 @@
 <script setup>
     import { ref } from 'vue';
 
-    const texto = ref('');
+    const letras = ref([]);
 
     const reset = (form) => {
         form.reset();
-        texto.value = '';
+        letras.value = [];
     };
 
     const enviar = (e) => {
-        console.log(texto.value);
+        console.log(letras.value);
         reset(e.target);
     };
 </script>
 
 <template>
-    <header v-show="texto">
-        <p>Mensaje:</p>
-        <p class="salto-linea">{{ texto }}</p>
+    <header v-show="letras.length > 0">
+        <p>Letras seleccionadas:</p>
+        <p>{{ letras }}</p>
     </header>
 
     <section>
         <form @submit.prevent="enviar">
-            <textarea v-model="texto"></textarea>
-            <div>
-                <button type="submit">
-                    Enviar
-                </button>
-            </div>
+            <label>
+                <input type="checkbox" value="A" v-model="letras">
+                A
+            </label>
+
+            <label>
+                <input type="checkbox" value="B" v-model="letras">
+                B
+            </label>
+
+            <label>
+                <input type="checkbox" value="C" v-model="letras">
+                C
+            </label>
         </form>
     </section>
 </template>
 
 <style scoped>
-    .salto-linea {
-        /* Forma de aplicar salto de línea con css en caso de existir */
-        white-space: pre-line;
-    }
+
 </style>
