940d0f0fde55179b68fb9cc1e5422b89979f4a81
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 16:21:00 -0400

    Palabras reservadas var vs let, para respetar el ámbito de las variables es recomendable usar let o const, ya que var no respeta los ámbitos ya que permite la redeclaración y es de ámbito global, esto puede causar fallas de performance

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
-let nombre = 'Manuel'; // Actualmente las variables se declaran con let y las constantes con const
-let nombre2 = 'Luna'; // La palabra let permite crear variables y modificar su valor, pero no redefinirlas, además son de ámbito local
+let nombre = 'Manuel'; // Alcance global
+
+if(true) 
+{
+    let nombre = 'Alejandro'; // Alcance local
+    
+    console.log(nombre);
+}
 
 console.log(nombre);
