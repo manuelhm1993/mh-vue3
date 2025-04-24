3a87e1f61f2a059407525d6cc8d536e84d05f161
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 10:54:22 -0400

    Si la función no recibe parámetros se invoca solo con su nombre, si la función recibe parámetros se invoca con los paréntesis y se pasa el parámetro correspondiente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
     // Para funciones un poco más complejas, se deben definir en el script y llamarse en el template
-    const saludo = () => alert('Hola desde vue');
+    const saludo = (nombre) => alert(`Hola ${nombre}`);
 </script>
 
 <template>
     <!-- Método abreviado -->
-    <button type="button" @click="saludo">
+    <button type="button" @click="saludo('Manuel Henriquez')">
         Haz click
     </button>
 </template>
