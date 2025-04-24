97a12f2fd65e24ca4f65ee87bd9ddd4c8ffdc3f1
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 12:01:35 -0400

    Manipular el envío de formulario y hacer reset

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,23 @@
     import { ref } from 'vue';
 
     const texto = ref('');
+
+    const enviar = (e) => {
+        console.log(texto.value);
+        e.target.reset();
+    };
 </script>
 
 <template>
     <p v-show="texto">Mensaje: {{ texto }}</p>
 
-    <form>
+    <form @submit.prevent="enviar">
         <!-- Vincular el valor del input con el objeto texto -->
         <input type="text" v-model="texto">
+
+        <button type="submit">
+            Enviar
+        </button>
     </form>
 </template>
 
