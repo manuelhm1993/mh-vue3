3a805ffb4cb8850e785b9ebb676f1939bd964e12
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 16:10:36 -0400

    Uso de la palabra reservada var

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+var nombre = 'Manuel'; // Antiguamente para definir variables se utilizaba la palabra reservada var
+var nombre = 'Luna'; // La palabra var permitia redefinir una variable y modificar su valor, también es de ámbito global
+
+console.log(nombre);
