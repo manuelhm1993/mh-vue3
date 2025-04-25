6e05edbb225b63b2269b6af6c25e4ef6d3ed4968
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-24 23:02:42 -0400

    Trabajando con objetos literales

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,21 @@
-// Declaración de función clásica
-const nombre = "Manuel";
-const apellido = "Henriquez";
-const edad = 31;
+// Declaración de un objeto
+const usuario = {
+    nombre: "Manuel",
+    apellido: "Henriquez",
+    edad: 31,
+    mascotas: ['Perro', 'Gato', 'Conejo']
+}; 
 
-// Concatenación clásica
-let data = "Hola, mi nombre es " + nombre + " " + apellido + " y mi edad es " + edad;
-
-// Uso de template strings
-data = `Hola, mi nombre es ${nombre} ${apellido} y mi edad es ${edad}`;
+// Accediento a las propiedades del objeto
+let data = `Hola, mi nombre es ${usuario.nombre} ${usuario.apellido} y mi edad es ${usuario.edad}`;
 
 console.log(data);
+
+// Asignando una propiedad nueva al objeto dinámicamente
+usuario.id = '0000000001';
+
+// Reasignando el valor de la propiedad nombre
+usuario.nombre = "Manuel A.";
+
+// Imprimir el objeto
+console.log(usuario);
