7dbfe1909223ec0a07e80b5735cd2e7edbef032c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-25 00:06:44 -0400

    Uso de async y await, es otra forma de usar las promesas en sustitución de then y catch, particularmente la prefiero y se trabajó con funciones autoinvocadas, no es un concepto que se use mucho, pero en este caso tuvo su utilidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,15 +22,25 @@ const getData = () => {
     // Las promesas esperan a que termine el proceso para luego ejecutar una acción
     return new Promise((resolve, reject) => {
         // Resolve indica que la promesa a concluido su ejecución y reject indica que ocurrió un error
-        setTimeout(() => (!true) ? resolve(data) : reject('Error. No se pudo acceder a la data'), 2000);
+        setTimeout(() => (true) ? resolve(data) : reject('Error. No se pudo acceder a la data'), 2000);
     });
 };
 
-// Recuperar la data en una promesa
-const response = getData();
+// Simulación de fetch: async indica que la función es asíncrona
+(async () => {
+    // Se usa try-catch para manejar los errores
+    try {
+        // Se usa await para recuperar el valor de la promesa una vez resuelta
+        const res = await getData();
 
-// El método then permite recuperar el valor de la promesa y catch captura una excepción
-response.then(data => console.log(data)).catch(e => console.log(e));
+        // Se imprime el valor
+        console.log(res);
+    } 
+    catch (error) {
+        // Se imprime el error
+        console.log(error);
+    }
+})(); // Función anónima auto-invocada - IIFE - Expresión de función ejecutada inmediatamente
 
 // Resto del flujo de ejecución
 console.log('Continúa el programa');
