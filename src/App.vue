8cd3aca1d70cf9916de3e3fdc59a64975c41e845
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-27 11:55:29 -0400

    Switch del darkmode, se usaron apply para hacer clases propias con estilos tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
+    import { ref } from 'vue';
     import AlertComponent from './components/AlertComponent.vue';
+
+    const app = document.querySelector('#app');
+    const botonDark = document.querySelector('button');
+    const dark = ref(false);
+
+    const switchDark = () => {
+        dark.value = !dark.value;
+        
+        (dark.value) ? addDark([app, botonDark]) : removeDark([app, botonDark]);
+    };
+
+    const addDark = (items) => items.forEach(item => {
+        item.classList.add('dark');
+    });
+
+    const removeDark = (items) => items.forEach(item => {
+        item.classList.remove('dark');
+    });
 </script>
 
 <template>
     <!-- Pasarle atributos literales al componente -->
     <AlertComponent class="bg-red-600" />
+
+    <button type="button" @click="switchDark" class="btn btn-light">Dark mode {{ (dark) ? 'on' : 'off' }}</button>
 </template>
 
 <style scoped>
--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -1,20 +1,15 @@
 <script setup>
-    // Si no se capturan los atributos, vue los asignará al contenedor padre
-    defineOptions({
-        inheritAttrs: false // Anula el comportamiento por defecto de la herencia y no aplica los estilos al contenedor padre
-    });
+// Si no se capturan los atributos, vue los asignará al contenedor padre
+defineOptions({
+    inheritAttrs: false // Anula el comportamiento por defecto de la herencia y no aplica los estilos al contenedor padre
+});
 </script>
 
 <template>
     <!-- Un componente heredado debe tener un contenedor padre -->
-    <div class="text-white">
-        <!-- Vincula los atributos del contenedor padre en este elemento -->
-        <div v-bind="$attrs">
-            Hola mundo
-        </div>
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
