b885ea4a20d53a6312617ba564216859c5b58754
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-27 09:36:31 -0400

    Emición de eventos desde el script, esto permite que se usen funciones manejadoras y es la manera más escalable, se definen los emisores igual que las props

--- src/components/DetailCourse.vue
+++ src/components/DetailCourse.vue
@@ -1,4 +1,8 @@
 <script setup>
+    // Definit el objeto emisor de eventos personalizados (forma correcta de hacerlo)
+    const emit = defineEmits(['decrementarCont']);
+
+    // Definir o recuperar las variables 
     const { course, cont } = defineProps({
         course: Object,
         cont: {
@@ -6,15 +10,18 @@
             default: 0
         }
     });
+
+    // Función para emitir un evento
+    const buttonClick = () => emit('decrementarCont', 2);
 </script>
 
 <template>
-    <h2>{{ course.title }} - {{ cont }}</h2>
+    <h2>{{ course.title }} ({{ cont }})</h2>
     <p>Profesor: {{ course.teacher }}</p>
     <p>Costo: ${{ course.price }}</p>
 
-    <!-- Para afectar objetos del componente padre es necesario emitir eventos desde el componente hijo -->
-    <button type="button" @click="$emit('decrementarCont', 2)">
+    <!-- Se puede emitir un evento desde una función manejadora -->
+    <button type="button" @click="buttonClick">
         Hazme click
     </button>
 </template>
