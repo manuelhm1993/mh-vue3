a8bdb309a99a00381948f8b1de25f4689e3ac14c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-27 12:17:27 -0400

    Reestructuración de las carpetas del proyecto, estilos personalizados css, se creó un componente para trabajar el darkmode personalizado a través de un botón

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup>
-    import { ref } from 'vue';
     import AlertComponent from './components/AlertComponent.vue';
-
-    const app = document.querySelector('#app');
-    const botonDark = document.querySelector('button');
-    const dark = ref(false);
-
-    const switchDark = () => {
-        dark.value = !dark.value;
-        
-        (dark.value) ? addDark([app, botonDark]) : removeDark([app, botonDark]);
-    };
-
-    const addDark = (items) => items.forEach(item => {
-        item.classList.add('dark');
-    });
-
-    const removeDark = (items) => items.forEach(item => {
-        item.classList.remove('dark');
-    });
+    import DarkModeComponent from './components/DarkModeComponent.vue';
 </script>
 
 <template>
-    <!-- Pasarle atributos literales al componente -->
-    <AlertComponent class="bg-red-600" />
-
-    <button type="button" @click="switchDark" class="btn btn-light">Dark mode {{ (dark) ? 'on' : 'off' }}</button>
+    <AlertComponent />
+    <DarkModeComponent />
 </template>
 
 <style scoped>
