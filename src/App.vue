03b048fa43e5a4d01c28759036921977d8777759
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-27 11:17:05 -0400

    Si no se han capturado atributos en el script, estos son almacenados dentro de la variable , se pueden vincular con v-bind, un componente siempre debe tener un contenedor padre y si se quiere evitar el comportamiento por defecto de inyectar atributos al contenedor padre, se usa defineOptions con el objeto y la propiedad inheritAttrs: false

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <script setup>
-    import { ref } from 'vue';
-    import DetailCourse from './components/DetailCourse.vue';
-
-    // Data simulada de una API
-    const courses = 
-    [
-        {
-            "title": "Vue desde cero",
-            "teacher": "Manuel Henriquez",
-            "price": 10
-        },
-        {
-            "title": "Laravel desde cero",
-            "teacher": "Manuel Henriquez",
-            "price": 20
-        },
-        {
-            "title": "PHP desde cero",
-            "teacher": "Manuel Henriquez",
-            "price": 30
-        },
-    ];
-
-    const cont = ref(5);
+    import AlertComponent from './components/AlertComponent.vue';
 </script>
 
 <template>
-    <input type="text" v-model.number="cont">
-    
-    <template v-for="course in courses">
-        <!-- Si el atributo se llama igual que la variable, se puede simplificar la llamada :cont -->
-        <DetailCourse :course="course" :cont
-            v-model:cont="cont"
-        /> <!-- Para enlazar un objeto del componente padre con el componente hijo se puede usar v-model -->
-    </template>
+    <!-- Pasarle atributos literales al componente -->
+    <AlertComponent class="bg-red-600" />
 </template>
 
 <style scoped>
---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+    // Si no se capturan los atributos, vue los asignará al contenedor padre
+    defineOptions({
+        inheritAttrs: false // Anula el comportamiento por defecto de la herencia y no aplica los estilos al contenedor padre
+    });
+</script>
+
+<template>
+    <!-- Un componente heredado debe tener un contenedor padre -->
+    <div class="text-white">
+        <!-- Vincula los atributos del contenedor padre en este elemento -->
+        <div v-bind="$attrs">
+            Hola mundo
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
