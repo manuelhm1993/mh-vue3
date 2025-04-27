8ab40d0e32ea80a5edca2203e4a972eb3d08f6fa
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-27 13:39:42 -0400

    Trabajo con múltiples slots, siempre habrá un principal y el resto deben ser nombrados con el atributo name, luego invocados a través de un template con su respectivo nombre

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 <template>
     <!-- Para cargar el slot se debe usar apertura y cierre de etiqueta -->
     <AlertComponent type="danger">
+        <!-- Contenido del slot secundario, se especifica su nombre -->
+        <template v-slot:title> <!-- Sintaxis alternativa: <template #title> -->
+            Alert danger
+        </template>
+
+        <!-- Contenido del slot principal -->
         Hola desde el alert
     </AlertComponent>
     
